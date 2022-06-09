7eb258ca9248a73805876cf5048427c7ad605bf6
joaquinallaio <joaco.allaio@gmail.com>
2022-06-09 15:50:00 -0300

    Optimization of predict value function

--- web_carol/gcloud.py
+++ web_carol/gcloud.py
@@ -49,11 +49,17 @@ def get_dataframe():
     df = pd.read_csv("medicamentos.csv", sep=';', encoding='latin-1')
     return df
 
-def get_prob_value(target,columna,thr):
+
+def get_prob_value(target,columna,thr,droga):
+
     med = get_dataframe()
     sims =[]
     method= jarowinkler_similarity
-    vocab= list(set(med[columna]))
+    if droga != "error":
+        vocab= list(set(med[med['Principio activo']==droga][columna]))
+    else:
+        vocab= list(set(med[columna]))
+
     for word in vocab:
         sims.append(method(target, word))
     if np.max(sims) > thr:
@@ -64,16 +70,26 @@ def get_prob_value(target,columna,thr):
 def predict(target):
     final_list = []
     for w in target:
-        droga_pred= get_prob_value(w ,'Principio activo',0.8)
-        gramaje_pred= get_prob_value(w,'Potencia',0.9)
-        unidad_pred= get_prob_value(w,'Unidad de potencia',0.8)
+        droga_pred= get_prob_value(w ,'Principio activo',0.8,"error")
+        gramaje_pred= get_prob_value(w,'Potencia',0.7,droga_pred)
+        if gramaje_pred != "error":
+            unidad_pred= get_prob_value(w,'Unidad de potencia',0.7,droga_pred)
+        else:
+            unidad_pred = "error"
 
         if droga_pred != "error":
             final_list.append(droga_pred)
+        else :
+            final_list.append(" ")
         if gramaje_pred != "error":
             final_list.append(gramaje_pred)
+        else :
+            final_list.append(" ")
         if unidad_pred != "error":
             final_list.append(unidad_pred)
+        else :
+            final_list.append(" ")
+
     return final_list
 
 def get_drugs(predicted_words):
